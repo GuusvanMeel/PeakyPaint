49bfcd76c38075b625e1a2423778beba7c5919c1
Maarten <92666704+Maarten0162@users.noreply.github.com>
2025-01-13 12:38:47 +0100

    save loading(soort van)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,9 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.IO;
 
 namespace DrawingApp
 {
@@ -10,8 +13,8 @@ namespace DrawingApp
     {
         private bool isDrawing = false; // Flag to track drawing state
         private Polyline currentLine; // The current line being drawn
-        private Brush selectedBrush = Brushes.Black; // The current selected brush color
-        Color selectedColor = Colors.Black;
+        private System.Windows.Media.Brush selectedBrush = System.Windows.Media.Brushes.Black; // The current selected brush color
+        System.Windows.Media.Color selectedColor = Colors.Black;
 
         public MainWindow()
         {
@@ -38,7 +41,7 @@ namespace DrawingApp
                         selectedBrush = new RadialGradientBrush(Colors.Red, Colors.Blue);
                         break;
                     case "Eraser":
-                        selectedBrush = Brushes.White; // You can modify this if needed
+                        selectedBrush = System.Windows.Media.Brushes.White; // You can modify this if needed
                         break;
                 }
                 int thickness = Convert.ToInt32(BrushSizeComboBox.SelectedItem);
@@ -82,7 +85,7 @@ namespace DrawingApp
         // Color Picker selection changed
         private void ColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<System.Windows.Media.Color?> e)
         {
-            selectedColor = (Color)e.NewValue;
+            selectedColor = (System.Windows.Media.Color)e.NewValue;
             selectedBrush = new SolidColorBrush(selectedColor);
         }
 
@@ -98,5 +101,62 @@ namespace DrawingApp
             }
             return null;
         }
+
+        private void SaveMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap((int)DrawingCanvas.ActualWidth, (int)DrawingCanvas.ActualHeight, 96, 96, System.Windows.Media.PixelFormats.Pbgra32);
+
+            renderTargetBitmap.Render(DrawingCanvas);
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                BitmapEncoder encoder = new BmpBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+
+                // Save the frame into a memory stream
+                encoder.Save(memoryStream);
+
+                // Convert to System.Drawing.Bitmap
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                using (System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(memoryStream))
+                {
+                    Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog
+                    {
+                        Filter = "Bitmap Files (*.bmp)|*.bmp"
+                    };
+
+                    if (saveFileDialog.ShowDialog() == true)
+                    {
+                        bitmap.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                    }
+                }
+            }
+        }
+                private void LoadMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            // Open an OpenFileDialog to select the image file
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Bitmap Files (*.bmp)|*.bmp"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Create a BitmapImage from the selected file
+                BitmapImage bitmapImage = new BitmapImage(new Uri(openFileDialog.FileName));
+
+                // Create an Image control to display the loaded image
+                System.Windows.Controls.Image loadedImage = new System.Windows.Controls.Image
+                {
+                    Source = bitmapImage,
+                    Width = DrawingCanvas.ActualWidth,
+                    Height = DrawingCanvas.ActualHeight
+                };
+
+                // Clear the canvas and add the image to it
+                DrawingCanvas.Children.Clear();
+                DrawingCanvas.Children.Add(loadedImage);
+            }
+        }
+
     }
 }
